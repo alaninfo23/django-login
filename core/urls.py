c26ea3d0c025a98b6eff75988cf462690776a3d2
José Alan dos Santos Rocha <js-alan@hotmail.com>
2026-06-03 13:47:32 -0300

    feat: user dropdown with logout and password change

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('accounts.urls')),
     path('assets/', include('assets.urls')),
+    path('password-change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
 ]
